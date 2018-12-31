98ca63bca177c28cca35b2f0b8821c521352c260
Zack McCauley <wpgraphix@gmail.com>
2018-12-31 15:46:58 -0700

    Added pending_removals_munki widget Moved pending_munki to pending_installs_munki Added en.json for pendining_removals

--- views/pending_removals_munki_widget.php
+++ views/pending_removals_munki_widget.php
@@ -1,12 +1,12 @@
 <div class="col-lg-4 col-md-6">
 
-	<div class="panel panel-default" id="pending-munki-widget">
+	<div class="panel panel-default" id="pending-removals-munki-widget">
 
-		<div class="panel-heading" data-container="body" data-i18n="[title]managedinstalls.widget.pending_munki.tooltip">
+		<div class="panel-heading" data-container="body" data-i18n="[title]managedinstalls.widget.pending_removals_munki.tooltip">
 
 			<h3 class="panel-title"><i class="fa fa-shopping-cart"></i>
-			    <span data-i18n="managedinstalls.widget.pending_munki.title"></span>
-			    <list-link data-url="/module/managedinstalls/listing/#pending_install"></list-link>
+			    <span data-i18n="managedinstalls.widget.pending_removals_munki.title"></span>
+			    <list-link data-url="/module/managedinstalls/listing/#pending_removal"></list-link>
 			</h3>
 
 		</div>
@@ -22,14 +22,14 @@
 $(document).on('appUpdate', function(e, lang) {
 
 
-	$.getJSON( appUrl + '/module/managedinstalls/get_pending_installs/munki', function( data ) {
+	$.getJSON( appUrl + '/module/managedinstalls/get_pending_removals/munki', function( data ) {
 
-        var box = $('#pending-munki-widget div.scroll-box').empty();
+        var box = $('#pending-removals-munki-widget div.scroll-box').empty();
 
 		if(data.length){
 			$.each(data, function(i,d){
 				var badge = '<span class="badge pull-right">'+d.count+'</span>',
-                    url = appUrl+'/module/managedinstalls/listing/'+d.name+'#pending_install',
+                    url = appUrl+'/module/managedinstalls/listing/'+d.name+'#pending_removal',
 					display_name = d.display_name || d.name;
 
 				box.append('<a href="'+url+'" class="list-group-item">'+display_name+' '+d.version+badge+'</a>');
