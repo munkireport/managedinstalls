6adec5a00458d8d6442b809d2fea38cc75e35f23
Arjen van Bochoven <nejra@me.com>
2019-07-07 11:35:35 +0200

    New listing format

---
+++ js/managedinstalls_functions.js
@@ -0,0 +1,55 @@
+// Global object to hold the variables
+var managedInstallsVariables = {
+    pkgName: '',
+    pkgVersion: ''
+}
+
+// Init function
+var initializeManagedInstalls = function(pkgName, pkgVersion){
+    // Save the variables to the global space so the filter can use them
+    managedInstallsVariables.pkgName = pkgName;
+    managedInstallsVariables.pkgVersion = pkgVersion;
+    if(pkgName){
+        // Set name on heading
+        $('h3>span:first').text(pkgName);
+
+        if(pkgVersion){
+            // Add version to heading
+            $('h3>span:first').text(pkgName + ' ('+pkgVersion+')');
+        }
+    }
+}
+
+// Filters
+var managedInstallsFilter = function(colNumber, d){
+
+    // Add where array to filter the results
+    d.where = [];
+
+    if(managedInstallsVariables.pkgName){
+        d.where.push({
+            table: 'managedinstalls',
+            column: 'name',
+            value: managedInstallsVariables.pkgName
+        });
+
+        if(managedInstallsVariables.pkgVersion){
+            d.where.push({
+                table: 'managedinstalls',
+                column: 'version',
+                value: managedInstallsVariables.pkgVersion
+            });
+        }
+    }
+}
+
+// Formatters
+var managedInstallStatus = function(colNumber, row){
+    // Show label depending on status
+    var col = $('td:eq('+colNumber+')', row),
+        status = col.text();
+    if(mr.statusFormat[status]){
+        status = '<span class = "label label-'+mr.statusFormat[status].type+'">'+status+'</span>'
+    }
+    col.html(status)
+}
