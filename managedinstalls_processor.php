4669c736af9d0608597fc4888c5119d66b1c97ee
Rod Christiansen <rodchristiansen@me.com>
2026-03-11 09:34:11 -0700

    Add YAML Support for Munki Data Format (#11)
    * Add YAML support alongside XML parsing

--- managedinstalls_processor.php
+++ managedinstalls_processor.php
@@ -5,21 +5,29 @@ use munkireport\processors\Processor;
 
 class Managedinstalls_processor extends Processor
 {
-    private $timestamp;
-
-    public function run($plist)
+    public function run($data)
     {
         $this->timestamp = date('Y-m-d H:i:s');
 
-        if (! $plist) {
+        if (! $data) {
             throw new Exception(
-                "Error Processing Request: No property list found", 1
+                "Error Processing Request: No data found", 1
             );
         }
 
-        $parser = new CFPropertyList();
-        $parser->parse($plist, CFPropertyList::FORMAT_XML);
-        if( ! $mylist = $parser->toArray()){
+        // Parse plist or YAML data
+        $trimmedData = ltrim($data);
+        if (strpos($trimmedData, '<?xml') === 0 ||
+            strpos($trimmedData, '<!DOCTYPE plist') !== false ||
+            strpos($trimmedData, '<plist') !== false) {
+            $parser = new CFPropertyList();
+            $parser->parse($data, CFPropertyList::FORMAT_XML);
+            $mylist = $parser->toArray();
+        } else {
+            $mylist = \Symfony\Component\Yaml\Yaml::parse($data);
+        }
+
+        if (! $mylist) {
             return;
         }
 
